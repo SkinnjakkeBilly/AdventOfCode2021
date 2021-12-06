ab827330141875a20128590464faf45da688350a
BIS\benbre <bendik.breidablikk@bisnode.com>
2021-12-06 18:51:17 +0100

    refactor D5T1

--- src/com/company/DayFive.java
+++ src/com/company/DayFive.java
@@ -32,11 +32,16 @@ public class DayFive {
             String Y1 = coordinates.get(c)[0].split(",")[1];
             String Y2 = coordinates.get(c)[1].split(",")[1];
 
-            if (Y1.equals(Y2)) {//horisontal
+            if (Y1.equals(Y2) || X1.equals(X2)) {//horisontal
                 if (Integer.parseInt(X1) > Integer.parseInt(X2)) {
                     String tmp = X2;
                     X2 = X1;
                     X1 = tmp;
+                }
+                if (Integer.parseInt(Y1) > Integer.parseInt(Y2)) {
+                    String tmp = Y2;
+                    Y2 = Y1;
+                    Y1 = tmp;
                 }
                 for (int i = Integer.parseInt(Y1); i <= Integer.parseInt(Y2); i++) {
                     for (int j = Integer.parseInt(X1); j <= Integer.parseInt(X2); j++) {
@@ -47,34 +52,17 @@ public class DayFive {
                     }
                 }
             }
-            if (X1.equals(X2)) { //Vertikal
-
-                if (Integer.parseInt(Y1) > Integer.parseInt(Y2)) {
-                    String tmp = Y2;
-                    Y2 = Y1;
-                    Y1 = tmp;
-                }
-                for (int i = Integer.parseInt(Y1); i <= Integer.parseInt(Y2); i++) {
-                    for (int j = Integer.parseInt(X1); j <= Integer.parseInt(X2); j++) {
-                        //board[i][Integer.parseInt(X1)]++;
-                        board[i][j]++;
-                        /*if (board[i][Integer.parseInt(X1)] > 1) {
-                            overlap++;
-                        }*/
-                    }
-                }
-            }
         }
         //Print ut brettet:
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 //System.out.println("board["+i+"]["+j+"]: "+board[i][j]);
-                System.out.print(board[i][j] + "\t");
+                //System.out.print(board[i][j] + "\t");
                 if (board[i][j] > 1) {
                     overlap++;
                 }
             }
-            System.out.println();
+            //System.out.println();
         }
         return overlap;
     }
