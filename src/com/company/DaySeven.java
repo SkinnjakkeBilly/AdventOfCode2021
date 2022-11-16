9e491f010314ac235daa16fade9a3b2eae117c20
breidablikkb <breidablikkb@dnb.com>
2022-11-16 16:19:37 +0100

    dayseven progrsees

--- src/com/company/DaySeven.java
+++ src/com/company/DaySeven.java
@@ -2,19 +2,32 @@ package com.company;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class DaySeven {
 
         ReadFile RF = new ReadFile("DaySeven");
         ArrayList<String> entries = new ArrayList<>();
+        String[] nums;
 
         public DaySeven() throws FileNotFoundException {
             RF.ReadString(entries);
+            Iterator<String> it = entries.iterator();
+            nums = it.next().split(",");
         }
 
-        public int taskOne(){
-
-            return 0;
+        public String taskOne(){
+            String tmp = "0";
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i+1; j < nums.length; j++) {
+                    if(Integer.parseInt(nums[i])>Integer.parseInt(nums[j])){
+                        tmp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = tmp;
+                    }
+                }
+            }
+            return nums[nums.length/2];
         }
 
     }
