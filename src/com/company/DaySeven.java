60e3e560be030d4cb8dbc70c7425ef40f2d5ce01
breidablikkb <breidablikkb@dnb.com>
2023-01-18 10:20:11 +0100

    task to riktig på eksempel, men feil på oppgaveinnput

--- src/com/company/DaySeven.java
+++ src/com/company/DaySeven.java
@@ -10,9 +10,10 @@ public class DaySeven {
     ArrayList<String> entries = new ArrayList<>();
     String[] nums;
     int median;
+    int snitt;
 
     public DaySeven(String filePath) throws FileNotFoundException {
-        RF = new ReadFile(filePath, "DaySevenEx");
+        RF = new ReadFile(filePath, "DaySeven");
         RF.ReadString(entries);
         Iterator<String> it = entries.iterator();
         nums = it.next().split(",");
@@ -31,28 +32,19 @@ public class DaySeven {
     }
 
     public int taskTwo() {
-        //BRUK SNITTET ISTEDEN. EG ER DUM :(
-        median = getMedian();
+        snitt = getSnitt();
+
         int length;
         int ret = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-
-            int crab = Integer.parseInt(nums[i]);
-            length = Math.abs(crab - median);
-            /*
-            For-løkken under kan byttes ut med den litt vakrere og mer effektive:
-            ret+=((length*length)+length)/2;
-            ...
-            Hadde man bare husket nok formler fra Høgskolen :P
-            Jeg lar den stå for å se om jeg kan få det til å funke med min opprinnelige magefølelse.
-             */
-            for(int j = 1; j<=length; j++){
-                ret+= j;
-            }
-            //93030609
+        for (String num : nums) {
+            int crab = Integer.parseInt(num);
+            length = Math.abs(crab - snitt);
+            ret += ((length * length) + length) / 2;
         }
+
         return ret;
+        //92676748
     }
 
     private void sortCrabs() {
@@ -69,14 +61,19 @@ public class DaySeven {
     }
 
     private int getMedian() {
-        int median;
         if (nums.length % 2 != 0) {
             median = nums.length / 2;
-
         } else {
-            //FUNKER IKKE! brukte bare median = nums.length / 2 i steden;
             median = ((Integer.parseInt(nums[(nums.length-1) / 2])) + (Integer.parseInt(nums[((nums.length+1) / 2)]))) / 2;
         }
         return median;
+    }
+
+    public int getSnitt(){
+        int sum = 0;
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+        return (int) Math.ceil((double)sum / nums.length);
     }
 }
